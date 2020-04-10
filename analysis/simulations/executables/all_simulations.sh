16917d8115960bf365f77b043cc4857e135297b4
efbonneville <e.f.bonneville@lumc.nl>
2020-04-10 19:31:27 +0200

    Set-up ready for large run of around 100 scenarios!

--- analysis/simulations/executables/all_simulations.sh
+++ analysis/simulations/executables/all_simulations.sh
@@ -5,7 +5,7 @@
 #SBATCH --mem=1G
 #SBATCH --partition=short
 #SBATCH -o ./analysis/other/out_err_messages/scenario%a_job%A.out
-#SBATCH --array=67,99
+#SBATCH --array=15,16 # These are the scenario numbers
 
 # run %2 so two running at any given times, each with 12 cores
 
