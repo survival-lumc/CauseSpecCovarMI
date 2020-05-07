c8af68f121501df6db67a60b6b871b34550a270f
efbonneville <e.f.bonneville@lumc.nl>
2020-05-07 17:41:00 +0200

    Added small AFT demo script

--- analysis/simulations/experiment_prediction-bias_tests.R
+++ analysis/simulations/experiment_prediction-bias_tests.R
@@ -44,13 +44,13 @@ ev2_pars <- list(
 
 # Generate a dataset based on scenario
 dat <- SimsCauseSpecCovarMiss::generate_dat(
-  n = 20000,
+  n = 500,
   X_type = "continuous", 
   r = 0.5, 
   ev1_pars = ev1_pars,
   ev2_pars = ev2_pars, 
   #rate_cens = baseline[baseline$state == "EFS", "rate"], 
-  rate_cens = 0.01,
+  rate_cens = 0.026,
   mech = "MAR", 
   p = 0,
   eta1 = -1
@@ -174,12 +174,15 @@ library(survival)
 survreg(Surv(t, eps) ~ 1, data = dat, 
         dist = "weibull")
 
+library(survival)
 
-# If you remove artificial censoring this is all correct
+
+# If you remove artificial censoring in generate_dat() this is all correct
 mod_cens <- survreg(Surv(t, eps == 0) ~ 1, data = dat, dist = "exponential")
 mod_rel <- survreg(Surv(t, eps == 1) ~ X + Z, data = dat, dist = "weibull")
 mod_nrm <- survreg(Surv(t, eps == 2) ~ X + Z, data = dat, dist = "weibull")
 
+# Without artif. censoring, this holds
 shape_rate <- function(mod) {
   
   mod_coefs <- mod$coefficients
@@ -189,13 +192,48 @@ shape_rate <- function(mod) {
   cat(shape, rate)
 }
 
-shape_rate(mod_cens)
+shape_rate(mod_cens)#0.026
 shape_rate(mod_rel)
 c(ev1_pars[[1]], ev1_pars[[2]])
 shape_rate(mod_nrm)
 c(ev2_pars[[1]], ev2_pars[[2]])
 
+library(prodlim)
+library(riskRegression)
 
+fit1 <- CSC(list(Hist(t, eps)) ~ X + Z, data=dat)
+
+library("flexsurv")
+
+tmat <- mstate::trans.comprisk(2, c("Rel", "NRM"))
+covs <- c("X", "Z")
+
+# Long format
+dat_msprepped <- mstate::msprep(time = c(NA, "t", "t"),
+                                status = c(NA, "ev1", "ev2"), 
+                                data = dat,
+                                trans = tmat,
+                                keep = covs) 
+
+# Expand covariates
+dat_expanded <- mstate::expand.covs(dat_msprepped, covs,
+                                    append = TRUE, longnames = F)
+
+
+test_flex <- flexsurvreg(Surv(time, status) ~ trans + shape(trans), 
+                         data = dat_msprepped,
+                         dist = "weibullPH")
+
+flexsurvreg(Surv(time, status) ~ X + Z, 
+            subset = (trans == 2),
+            data = dat_msprepped,
+            dist = "weibullPH")
+
+
+
+test_flex
+
+coxph(Surv(t, eps) ~ X + Z, data = dat, id = 1:nrow(dat))
 
 # Try with cfc
 formul <- Surv(t, eps) ~ X
@@ -212,6 +250,43 @@ ret <- cfc.survreg(formul, bmt[1:300, ], bmt[-(1:300), ]
 
 bmt$cause
 
+# Try with onko kiss dara
+#install.packages("../../software-other/compeir_1.0.tar.gz", repos = NULL, type="source")
+library("compeir")
+data("okiss")
+
+# Mod 
+
+#
+library(tidyverse)
+library(survival)
+
+dato <- okiss %>% 
+  mutate(eps = case_when(
+    status == 11 ~ 0,
+    status == 1 ~ 1,
+    status == 2 ~ 2,
+    status == 7 ~ 2,
+  ))
+
+# mod BSI
+survreg(Surv(time, eps == 0) ~ 1,
+        data = dato, dist = "lognormal")
+
+survreg(Surv(time, eps == 1) ~ 1,
+        data = dato, dist = "lognormal")
+
+survreg(Surv(time, eps == 2) ~ 1,
+        data = dato, dist = "lognormal")
+
+
+survreg(Surv(time, eps == 1) ~ sex + allo,
+        data = dato, dist = "lognormal")
+
+survreg(Surv(time, eps == 2) ~ sex + allo,
+        data = dato, dist = "lognormal")
+
+
 # Probability missing demo ------------------------------------------------
 
 
@@ -221,7 +296,7 @@ bmt$cause
 
 etas <- c(0, -.25, -0.5, -.75, -1, -2)
 
-Z <- rnorm(1000)
+Z <- seq(-3, 3, by = 0.01)
 
 prob_plots <- purrr::map_dfr(
   etas,
@@ -235,9 +310,100 @@ prob_plots <- purrr::map_dfr(
 prob_plots %>% 
   dplyr::mutate(eta1 = factor(eta1)) %>% 
   ggplot(aes(Z, prob, col = eta1, group = eta1)) +
-  geom_line(size = 1) +
+  geom_line(size = 1.5) +
+  coord_cartesian(expand = 0, ylim = c(0, 1)) +
+  ylab(bquote(P(R[X] ~ "= 1"*"|"*Z))) +
+  guides(
+    col = guide_legend(title = expression(eta[1]))
+  ) +
+  scale_color_brewer(palette = "Dark2") 
+
+
+# Facetted plot
+
+test <- purrr::map_dfr(
+  etas,
+  ~ generate_dat(
+    n = 500,
+    X_type = "continuous", 
+    r = 0.5, 
+    ev1_pars = ev1_pars,
+    ev2_pars = ev2_pars, 
+    rate_cens = baseline[baseline$state == "EFS", "rate"], 
+    mech = "MAR", 
+    p = 0.5,
+    eta1 = .x
+  ) %>% 
+    dplyr::mutate("eta1" = .x)
+)
+
+expression(paste(eta, as.character(etas)))
+
+
+test %>% 
+  dplyr::mutate(
+    miss_ind = factor(miss_ind),
+    eta1 = factor(eta1, levels = as.character(etas),
+                  labels = paste0("eta1 = ", as.character(etas)))
+  ) %>% 
+  ggplot(aes(Z, X_orig, col = miss_ind)) +
+  geom_point(alpha = 0.75, size = 2) +
+  scale_colour_manual("Missing indicator",
+                      labels = c("Observed", "Missing"),
+                      values = c(9, 6)) +
+  xlab("Z") +
+  ylab("X") +
+  facet_wrap(. ~ eta1) +
+  theme(legend.position = "top")
+
+
+
+# Example with rnorm around 5
+Z <- rnorm(1000, mean = 5)
+plot(Z, plogis(-5 + Z))
+
+mean(plogis(Z))
+
+
+# Lets do same procedure with t
+etas <- c(0, -.25, -0.5, -.75, -1, -2)
+
+t <- seq(0.01, max(dat$t), by = 0.1)
+
+prob_plots <- purrr::map_dfr(
+  etas,
+  ~ cbind.data.frame(
+    "t" = dat$t, 
+    "prob" = plogis(.x * scale(log(dat$t))),
+    "eta1" = .x
+  )
+)
+
+prob_plots %>% 
+  dplyr::mutate(eta1 = factor(eta1)) %>% 
+  ggplot(aes(t, prob, col = eta1, group = eta1)) +
+  geom_line(size = 1.5) +
+  coord_cartesian(expand = 0, ylim = c(0,1)) +
+  ylab(bquote(P(R[X] ~ "= 1"*"|"*T[stand]))) +
+  guides(
+    col = guide_legend(title = expression(eta[1]))
+  ) +
+  xlab("T") +
+  scale_color_brewer(palette = "Dark2") 
+
+
+prob_plots %>% 
+  dplyr::mutate(eta1 = factor(eta1)) %>% 
+  ggplot(aes(scale(log(t)), prob, col = eta1, group = eta1)) +
+  geom_line(size = 1.5) +
   theme_bw() +
-  coord_cartesian(expand = 0.1)
+  coord_cartesian(expand = 0.1) +
+  ylab(bquote(P(R[X] ~ "= 1"*"|"*T[stand]))) +
+  guides(
+    col = guide_legend(title = expression(eta[1]))
+  ) +
+  xlab(bquote(T[stand])) + 
+  scale_color_brewer(palette = "Dark2")
 
 
 # Facetted plot
@@ -269,14 +435,177 @@ test %>%
   scale_colour_manual("Missing indicator",
                       labels = c("Observed", "Missing"),
                       values = c(9, 6)) +
-  xlab("t") +
+  xlab("T") +
+  ylab("X") +
   facet_wrap(. ~ eta1) +
-  theme_bw()
+  theme(legend.position = "top")
 
 
 
-# Example with rnorm around 5
-Z <- rnorm(1000, mean = 5)
-plot(Z, plogis(-5 + Z))
+# Plot illust haz ---------------------------------------------------------
 
-mean(plogis(Z))
+
+
+# Plots of cumulative incidences and hazards
+theme_set(theme_bw(base_size = 14))
+library(tidyverse)
+
+
+combo = data.frame(
+  "val_X" = 0, 
+  "val_Z" = 0
+)                  
+
+ev1_pars = list(
+  "a1" = 0.58, 
+  "h1_0" = 0.19,
+  "b1" = 0, 
+  "gamm1" = 0
+)    
+
+ev2_pars = list(
+  "a2" = 0.53, 
+  "h2_0" = 0.21, 
+  "b2" = 0, 
+  "gamm2" = 0
+)
+
+
+cuminc_similar <- get_true_cuminc(ev1_pars, ev2_pars, 
+                combo, 
+                times = seq(0, 10, by = 0.25)) %>% 
+  pivot_longer(true_pstate2:true_pstate1,
+               names_to = "state",
+               values_to = "prob") %>% 
+  ggplot(aes(times, prob, col = state,
+             linetype = state)) +
+  geom_line(size = 1.5, alpha = .75) +
+  scale_colour_manual(
+    "State", 
+    values = c(1, 2, 3),
+    labels = c("EFS", "REL", "NRM")
+  ) +
+  scale_linetype_manual("State", values = c("dotted", "solid","solid"),
+                        labels = c("EFS", "REL", "NRM")) +
+  theme(legend.position = "bottom",
+        plot.title = element_text(hjust = 0.5)) +
+  
+  # Title and labs
+  xlab("Time") + 
+  ylab("Probability")  +
+  ggtitle("haz_shape = 'similar'")
+
+
+t <- seq(0.01, 10, by = 0.1)
+
+lam1 <- ev1_pars$h1_0
+lam2 <- ev2_pars$h2_0
+
+haz1 <- haz_weib(alph = ev1_pars$a1, lam = lam1, t = t) 
+haz2 <- haz_weib(alph = ev2_pars$a2, lam = lam2, t = t) 
+haz_cens <- haz_weib(alph = 1, lam = 0.14, t = t)
+
+
+haz_similar <- cbind.data.frame(
+  "t" = t,
+  "haz1" = haz1,
+  "haz2" = haz2,
+  "haz3" = haz_cens
+) %>% 
+  gather("event", "hazard", .data$haz1, .data$haz2, .data$haz3) %>% 
+  ggplot(aes(.data$t, .data$hazard, col = event,
+             linetype = event)) +
+  geom_line(size = 1.5, alpha = 0.7) +
+  xlab("Time") +
+  ylab("Hazard") +
+  scale_color_manual(
+    "State", 
+    values = c(2, 3, 1),
+    labels = c("REL", "NRM", "EFS")
+  ) +
+  scale_linetype_manual("State", values = c("solid", "solid","dotted"),
+                        labels = c("REL", "NRM", "EFS")) + 
+  theme(legend.position = "bottom")
+
+
+
+
+
+ev1_pars = list(
+  "a1" = 1.5, 
+  "h1_0" = 0.04,
+  "b1" = 0, 
+  "gamm1" = 0
+)    
+
+ev2_pars = list(
+  "a2" = 0.53, 
+  "h2_0" = 0.21, 
+  "b2" = 0, 
+  "gamm2" = 0
+)
+
+
+cuminc_diff <-get_true_cuminc(ev1_pars, ev2_pars, 
+                combo, 
+                times = seq(0, 10, by = 0.25)) %>% 
+  pivot_longer(true_pstate2:true_pstate1,
+               names_to = "state",
+               values_to = "prob") %>% 
+  ggplot(aes(times, prob, col = state,
+             linetype = state)) +
+  geom_line(size = 1.5, alpha = .75) +
+  scale_colour_manual(
+    "State", 
+    values = c(1, 2, 3),
+    labels = c("EFS", "REL", "NRM")
+  ) +
+  scale_linetype_manual("State", values = c("dotted", "solid","solid"),
+                        labels = c("EFS", "REL", "NRM")) +
+  theme(legend.position = "bottom",
+        plot.title = element_text(hjust = 0.5)) +
+  
+  # Title and labs
+  xlab("Time") + 
+  ylab("Probability") +
+  ggtitle("haz_shape = 'different'")
+
+t <- seq(0.01, 10, by = 0.1)
+
+lam1 <- ev1_pars$h1_0
+lam2 <- ev2_pars$h2_0
+
+haz1 <- haz_weib(alph = ev1_pars$a1, lam = lam1, t = t) 
+haz2 <- haz_weib(alph = ev2_pars$a2, lam = lam2, t = t) 
+haz_cens <- haz_weib(alph = 1, lam = 0.14, t = t)
+
+haz_diff <- cbind.data.frame(
+  "t" = t,
+  "haz1" = haz1,
+  "haz2" = haz2,
+  "haz3" = haz_cens
+) %>% 
+  gather("event", "hazard", .data$haz1, .data$haz2, .data$haz3) %>% 
+  ggplot(aes(.data$t, .data$hazard, col = event,
+             linetype = event)) +
+  geom_line(size = 1.5, alpha = 0.7) +
+  xlab("Time") +
+  ylab("Hazard") +
+  scale_color_manual(
+    "State", 
+    values = c(2, 3, 1),
+    labels = c("REL", "NRM", "EFS")
+  ) +
+  scale_linetype_manual("State", values = c("solid", "solid","dotted"),
+                        labels = c("REL", "NRM", "EFS")) + 
+  theme(legend.position = "bottom")
+
+
+
+# library ggpubr
+library(ggpubr)
+
+ggarrange(cuminc_similar, cuminc_diff,
+          haz_similar, haz_diff, ncol = 2, nrow = 2, 
+          common.legend = T,
+          legend = "bottom")
