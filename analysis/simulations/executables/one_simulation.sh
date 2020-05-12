3e2a31839fc8700e1e0272965ea9aeace9bd5da9
efbonneville <e.f.bonneville@lumc.nl>
2020-05-12 13:43:34 +0200

    Change fix of scenario, for continuous less reps needed

--- analysis/simulations/executables/one_simulation.sh
+++ analysis/simulations/executables/one_simulation.sh
@@ -4,7 +4,7 @@
 #SBATCH --time=01:15:00 
 #SBATCH --mem=4G 
 #SBATCH -o ./analysis/other/out_err_messages/job%A_replicate%a.out
-#SBATCH --array=1-160:8 # this is the array of replicates then 3 in ':3' is the batch size, works for continous, change 60 to 30; no %30 for now
+#SBATCH --array=1-625:5  #1-160:8 # this is the array of replicates then 3 in ':3' is the batch size, works for continous, change 60 to 30; no %30 for now
 
 # Read in args
 scenario=$1 # First argument, the scenario
@@ -20,15 +20,19 @@ module load statistical/R/3.6.2/gcc.8.3.1.lua
 i=$i
 
 # Batch size and end of sequence - minus one since we only want a batch of x
-batch_size=8
+batch_size=5
 end=$(($replicate + $batch_size - 1)) # double brackets is called 'arithmetic expansion', need dollar sign before though
+
+
+# Add an if statement for size of n_sim
+
 
 # Begin sequence
 seqo=$(seq $replicate $end)
 
 for i in $seqo
 do
-  if [ $i -gt 160 ] # Stop if index is greater than (-gt) n_sim
+  if [ $i -gt 625 ] # Stop if index is greater than (-gt) n_sim, 160 for n = 200 and 625 for n = 500
   then
       break
   fi
